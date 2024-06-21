cedff929c51676f3bb7af925e1af33e68620dc56
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-21 10:05:47 -0700

    TypeTrek Day 5: Improve the Exercise details interface

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """Runs flask application"""
-
+import os
 from web_flask import create_app
 from sqlalchemy import create_engine
 from models.base_model import Base, DATABASE_URI
 from models.user import User
 from models.base_model import BaseModel
+
+# Check if MySQL service is running
+if os.system('sudo service mysql status') != 0:
+    # If not running, start MySQL service
+    os.system('sudo service mysql start')
 
 app = create_app()
 
