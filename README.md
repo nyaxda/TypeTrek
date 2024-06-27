13e987a3856205718b7598833b3e913bb56003e3
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-27 06:27:07 -0700

    TypeTrek Day 9: Code finalization and updating of README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # TypeTrek
 
 TypeTrek is a comprehensive typing practice and improvement application designed to help users enhance their typing skills. With a focus on user engagement and detailed performance tracking, TypeTrek offers a range of features including interactive typing exercises, real-time feedback, and progress reporting.
@@ -6,12 +7,62 @@ TypeTrek is a comprehensive typing practice and improvement application designed
 - [Features](#features)
 - [Installation](#installation)
 - [Usage](#usage)
+- [Project Structure](#project-structure)
 - [Contributing](#contributing)
 - [License](#license)
 
 ## Features
-- User Authentication: Register and log in to save your progress.
-- Typing Exercises: Select from a variety of typing exercises designed to improve speed and accuracy.
-- Real-Time Feedback: Receive immediate feedback on your typing performance.
-- Progress Tracking: View your typing history and track improvements over time.
-- Report Generation: Generate detailed reports of your typing performance.
+- **User Authentication**: Register and log in to save your progress.
+- **Typing Exercises**: Select from a variety of typing exercises designed to improve speed and accuracy.
+- **Real-Time Feedback**: Receive immediate feedback on your typing performance.
+- **Progress Tracking**: View your typing history and track improvements over time.
+- **Report Generation**: Generate detailed reports of your typing performance.
+
+## Installation
+
+1. **Clone the repository:**
+   ```
+   git clone https://github.com/nyaxda/TypeTrek.git
+   cd typetrek
+   ```
+2. **Navigate to the TypeTrek directory:**
+```
+	cd TypeTrek
+```
+
+3. **Install the required dependencies:**
+```
+	pip install -r requirements.txt
+```
+
+## Usage
+To start using TypeTrek, follow these instructions:
+
+1.  Run the application:
+```
+python run.py
+```
+
+2.  Open your web browser and navigate to  `http://localhost:5000`.
+    
+3.  Register for an account or log in to start practicing your typing skills.
+
+## Contributing
+
+Contributions to TypeTrek are welcome! Here's how you can contribute:
+
+1.  Fork the repository.
+    
+2.  Create a new branch for your feature (`git checkout -b feature/AmazingFeature`).
+    
+3.  Commit your changes (`git commit -m 'Add some AmazingFeature'`).
+    
+4.  Push to the branch (`git push origin feature/AmazingFeature`).
+    
+5.  Open a pull request.
+    
+
+Please make sure to add tests as appropriate.
+
+## License
+TypeTrek is released under the ALX Africa License.
--- web_flask/static/scripts/exercise_detail.js
+++ web_flask/static/scripts/exercise_detail.js
@@ -1,152 +1,154 @@
-let startTime;
-const content = "{{ exercise.content }}";
-const userInput = document.getElementById('user-input');
-const exerciseText = document.getElementById('exercise-text');
-const accuracyElem = document.getElementById('accuracy');
-const wpmElem = document.getElementById('wpm');
-const spmElem = document.getElementById('spm');
-const nextExerciseBtn = document.getElementById('next-exercise-btn');
-const backBtn = document.getElementById('back-btn');
-const cancelBtn = document.getElementById('cancel-btn');
-const redoExerciseBtn = document.getElementById('redo-exercise-btn');
-const completionButtons = document.getElementById('completion-buttons');
+document.addEventListener('DOMContentLoaded', () => {
+    let startTime;
+    const content = "{{ exercise.content }}";
+    const userInput = document.getElementById('user-input');
+    const exerciseText = document.getElementById('exercise-text');
+    const accuracyElem = document.getElementById('accuracy');
+    const wpmElem = document.getElementById('wpm');
+    const spmElem = document.getElementById('spm');
+    const nextExerciseBtn = document.getElementById('next-exercise-btn');
+    const backBtn = document.getElementById('back-btn');
+    const cancelBtn = document.getElementById('cancel-btn');
+    const redoExerciseBtn = document.getElementById('redo-exercise-btn');
+    const completionButtons = document.getElementById('completion-buttons');
 
-userInput.addEventListener('input', function() {
-    if (!startTime) {
-        startTime = new Date();
-    }
+    userInput.addEventListener('input', function() {
+        if (!startTime) {
+            startTime = new Date();
+        }
 
-    const userText = userInput.value;
-    const accuracy = calculateAccuracy(content, userText);
-    const timeElapsed = (new Date() - startTime) / 1000 / 60; // time in minutes
-    const wordsTyped = userText.split(' ').filter(Boolean).length;
-    const strokesTyped = userText.length;
-    const wpm = (wordsTyped / timeElapsed).toFixed(2);
-    const spm = (strokesTyped / timeElapsed).toFixed(2);
+        const userText = userInput.value;
+        const accuracy = calculateAccuracy(content, userText);
+        const timeElapsed = (new Date() - startTime) / 1000 / 60; // time in minutes
+        const wordsTyped = userText.split(' ').filter(Boolean).length;
+        const strokesTyped = userText.length;
+        const wpm = (wordsTyped / timeElapsed).toFixed(2);
+        const spm = (strokesTyped / timeElapsed).toFixed(2);
 
-    accuracyElem.textContent = `${accuracy}%`;
-    wpmElem.textContent = wpm;
-    spmElem.textContent = spm;
+        accuracyElem.textContent = `${accuracy}%`;
+        wpmElem.textContent = wpm;
+        spmElem.textContent = spm;
 
-    updateExerciseText(content, userText);
+        updateExerciseText(content, userText);
 
-    if (userText.length >= content.length) {
-        userInput.disabled = true;
-        completionButtons.style.display = 'flex';
-        nextExerciseBtn.disabled = false;
-        cancelBtn.style.display = 'none';
-    }
-});
-
-function calculateAccuracy(content, userText) {
-    const contentChars = content.split('');
-    const userChars = userText.split('');
-    let correctChars = 0;
-
-    contentChars.forEach((char, index) => {
-        if (userChars[index] === char) {
-            correctChars++;
+        if (userText.length >= content.length) {
+            userInput.disabled = true;
+            completionButtons.style.display = 'flex';
+            nextExerciseBtn.disabled = false;
+            cancelBtn.style.display = 'none';
         }
     });
 
-    return ((correctChars / contentChars.length) * 100).toFixed(2);
-}
+    function calculateAccuracy(content, userText) {
+        const contentChars = content.split('');
+        const userChars = userText.split('');
+        let correctChars = 0;
 
-function updateExerciseText(content, userText) {
-    let updatedText = '';
-    for (let i = 0; i < content.length; i++) {
-        if (i < userText.length) {
-            if (content[i] === userText[i]) {
-                updatedText += `<span class="correct">${content[i]}</span>`;
-            } else {
-                updatedText += `<span class="incorrect">${content[i]}</span>`;
+        contentChars.forEach((char, index) => {
+            if (userChars[index] === char) {
+                correctChars++;
             }
-        } else {
-            updatedText += `<span>${content[i]}</span>`;
-        }
+        });
+
+        return ((correctChars / contentChars.length) * 100).toFixed(2);
     }
-    exerciseText.innerHTML = updatedText;
-}
 
-function saveProgress(accuracy, wpm, spm) {
-    return fetch('{{ url_for('main.save_progress') }}', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            exercise_id: {{ exercise.id }},
-            accuracy: accuracy,
-            wpm: wpm,
-            spm: spm
-        })
-    }).then(response => response.json())
-      .then(data => {
-        console.log('Progress saved:', data);
-      }).catch((error) => {
-        console.error('Error:', error);
-      });
-}
-
-nextExerciseBtn.addEventListener('click', function() {
-    const accuracy = accuracyElem.textContent.replace('%', '');
-    const wpm = wpmElem.textContent;
-    const spm = spmElem.textContent;
-    saveProgress(accuracy, wpm, spm).then(() => {
-        fetch('{{ url_for('main.next_exercise') }}?current_exercise_id={{ exercise.id }}&difficulty_level={{ exercise.difficulty_level }}')
-            .then(response => response.json())
-            .then(data => {
-                if (data.next_exercise_id) {
-                    window.location.href = "{{ url_for('main.exercise', exercise_id=0) }}".replace('0', data.next_exercise_id);
+    function updateExerciseText(content, userText) {
+        let updatedText = '';
+        for (let i = 0; i < content.length; i++) {
+            if (i < userText.length) {
+                if (content[i] === userText[i]) {
+                    updatedText += `<span class="correct">${content[i]}</span>`;
                 } else {
-                    window.location.href = "{{ url_for('main.exercises') }}";
+                    updatedText += `<span class="incorrect">${content[i]}</span>`;
                 }
-            });
-    });
-});
-
-backBtn.addEventListener('click', function() {
-    const accuracy = accuracyElem.textContent.replace('%', '');
-    const wpm = wpmElem.textContent;
-    const spm = spmElem.textContent;
-    saveProgress(accuracy, wpm, spm).then(() => {
-        window.location.href = "{{ url_for('main.exercises') }}";
-    });
-});
-
-cancelBtn.addEventListener('click', function() {
-    if (confirm('Are you sure you want to cancel the exercise?')) {
-        window.location.href = "{{ url_for('main.exercises') }}";
+            } else {
+                updatedText += `<span>${content[i]}</span>`;
+            }
+        }
+        exerciseText.innerHTML = updatedText;
     }
-});
 
-redoExerciseBtn.addEventListener('click', function() {
-    userInput.value = '';
-    accuracyElem.textContent = '0%';
-    wpmElem.textContent = '0';
-    spmElem.textContent = '0';
-    exerciseText.innerHTML = content;
-    userInput.disabled = false;
-    startTime = null;
-    completionButtons.style.display = 'none';
-    nextExerciseBtn.disabled = true;
-    cancelBtn.style.display = 'inline-block';
-});
+    function saveProgress(accuracy, wpm, spm) {
+        return fetch('{{ url_for('main.save_progress') }}', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                exercise_id: {{ exercise.id }},
+                accuracy: accuracy,
+                wpm: wpm,
+                spm: spm
+            })
+        }).then(response => response.json())
+        .then(data => {
+            console.log('Progress saved:', data);
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+    }
 
-// Prevent pasting in the textarea
-userInput.addEventListener('paste', function(e) {
-    e.preventDefault();
-});
+    nextExerciseBtn.addEventListener('click', function() {
+        const accuracy = accuracyElem.textContent.replace('%', '');
+        const wpm = wpmElem.textContent;
+        const spm = spmElem.textContent;
+        saveProgress(accuracy, wpm, spm).then(() => {
+            fetch('{{ url_for('main.next_exercise') }}?current_exercise_id={{ exercise.id }}&difficulty_level={{ exercise.difficulty_level }}')
+                .then(response => response.json())
+                .then(data => {
+                    if (data.next_exercise_id) {
+                        window.location.href = "{{ url_for('main.exercise', exercise_id=0) }}".replace('0', data.next_exercise_id);
+                    } else {
+                        window.location.href = "{{ url_for('main.exercises') }}";
+                    }
+                });
+        });
+    });
 
-// Prevent selecting and copying the exercise content
-exerciseText.addEventListener('copy', function(e) {
-    e.preventDefault();
-});
+    backBtn.addEventListener('click', function() {
+        const accuracy = accuracyElem.textContent.replace('%', '');
+        const wpm = wpmElem.textContent;
+        const spm = spmElem.textContent;
+        saveProgress(accuracy, wpm, spm).then(() => {
+            window.location.href = "{{ url_for('main.exercises') }}";
+        });
+    });
 
-exerciseText.addEventListener('mousedown', function(e) {
-    e.preventDefault();
-});
+    cancelBtn.addEventListener('click', function() {
+        if (confirm('Are you sure you want to cancel the exercise?')) {
+            window.location.href = "{{ url_for('main.exercises') }}";
+        }
+    });
 
-exerciseText.addEventListener('selectstart', function(e) {
-    e.preventDefault();
+    redoExerciseBtn.addEventListener('click', function() {
+        userInput.value = '';
+        accuracyElem.textContent = '0%';
+        wpmElem.textContent = '0';
+        spmElem.textContent = '0';
+        exerciseText.innerHTML = content;
+        userInput.disabled = false;
+        startTime = null;
+        completionButtons.style.display = 'none';
+        nextExerciseBtn.disabled = true;
+        cancelBtn.style.display = 'inline-block';
+    });
+
+    // Prevent pasting in the textarea
+    userInput.addEventListener('paste', function(e) {
+        e.preventDefault();
+    });
+
+    // Prevent selecting and copying the exercise content
+    exerciseText.addEventListener('copy', function(e) {
+        e.preventDefault();
+    });
+
+    exerciseText.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+    });
+
+    exerciseText.addEventListener('selectstart', function(e) {
+        e.preventDefault();
+    });
 });
